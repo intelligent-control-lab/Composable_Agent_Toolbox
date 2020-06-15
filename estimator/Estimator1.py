62035ec192e7274fd4dc4c116282f5379906d5d8
Jaskaran <jaskarag@andrew.cmu.edu>
2020-06-15 14:44:59 -0400

    added new file called Estimator1.py

---
+++ estimator/Estimator1.py
@@ -0,0 +1,112 @@
+import math
+import numpy as np
+from abc import ABCMeta, abstractmethod
+
+
+class Estimator_Base():
+	__metaclass__ = ABCMeta
+
+	estimator_name = ''
+	estimator_type = '' # EKF,UKF,RLS etc.
+
+	
+	@abstractmethod
+	def __init__(self):
+		pass
+	
+
+	@abstractmethod
+	def estimate(self):
+		'''
+		This function calculates the next state  each time step
+		'''
+		pass
+
+	@abstractmethod
+	def reset(self):
+		'''
+		return TRUE for success, FALSE for failure
+		'''
+		pass
+
+	def get_name(self):
+		return self.estimator_name
+
+	def get_type(self):
+		return self.estimator_type
+
+
+class Zero_Estimator(Estimator_Base):
+	'''
+	This is a blank estimator class.
+	The Zero_Estimator always returns 0 as estimator output.
+	'''
+	def __init__(self, spec, model):
+		self.name = 'Zero'
+		self.type = 'None'
+		self.spec = spec
+		self.model = model
+
+	def reset(self):
+		return True
+
+	def estimate(self, dt, x, goal_x, est_params):
+		return np.zeros(self.model.x_shape)
+
+
+class Estimator(Estimator_Base):
+	'''
+    This is the unified estimator class used in Agent Class.
+    Estimator specs should be given in the following format:
+    {'Extended Kalman Filter': EFK, 'Unscented Kalman Filter': 'UKF'}
+    The estimator key can be ignored if it's not used, it will be filled with an zero estimator automatically.
+    
+    E.g. EKF Estimator specs:
+    {"Extended Kalman filter": "EKF", "params": {"Rww": [1,2], "Rvv": [0, 0]}}
+    ''' 
+
+	def __init__(self,spec,model):
+		self.spec  = spec
+		self.model = model
+
+		if 'EKF' in self.spec:
+			self.EKF = globals()[self.spec['EKF']](self.spec['params'],self.model)
+		else:
+			self.EKF = Zero_Estimator(self.spec,self.model)
+
+
+
+class Extended_Kalman_Filter(Estimator_Base):
+
+	def __init__(self,estimator_params,model):
+		self.name = 'EKF'
+		self.params = estimator_params
+		self.model  = model
+		self.Rww    = estimator_params['Rww']
+		self.Rvv    = estimator_params['Rvv']
+
+	def param_tuning(self,params):
+		self.Rww = params['Rww']
+		self.Rvv = params['Rvv']
+		return True
+
+	def estimate(self,posterior_pos,posterior_state_cov,z_meas):
+		priori_pos               = robot_model.forward_propagate_dynamics(posterior_state)      				; # need to call dynamics of the paritcular robot model
+		A                        = robot_model.linearization_forward_dynamics(posterior_state)  				; # need to call dynamics of the paritcular robot model
+		priori_cov               = np.matmul(np.matmul(A,posterior_state_cov),np.transpose(A)) + self.Rww      	;   
+		n                        = len(posterior_pos)                                           				;
+		I                        = np.identity(n)                                               				;
+		H                        = robot_model.linearization_measurement_function(priori_pos)   				; # need to call dynamics of the paritcular robot model
+		innovation               = z_meas - robot_model.measurement_function(priori_pos)        				; # need to call dynamics of the paritcular robot model
+		S                        = np.matmul(np.matmul(H,priori_cov),np.transpose(H)) + self.Rvv  				;
+		Sinv                     = np.linalg.inv(S)																;
+		K                        = np.matmul(priori_cov,np.transpose(H))*Sinv                   				;
+		next_posterior_state     = priori_pos + np.matmul(K,innovation)                         				;
+		next_posterior_state_cov = np.matmul(I-(np.matmul(K,H)),priori_cov)                     				;
+		return next_posterior_state,next_posterior_state_cov
+
+	def reset(self):
+		return True
+
+
+
