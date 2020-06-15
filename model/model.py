66fd3202a305325b00ce2f3e404275bb40c4f9c4
cnoren <cnoren@andrew.cmu.edu>
2020-06-15 15:00:14 -0400

    Still work to do, added the evaluate and discretize functions

--- model/model.py
+++ model/model.py
@@ -1,20 +1,30 @@
+
+# Declare imports
 import numpy as np
 import sympy as sp
+import math as m
 
 class model(object):
-    def __init__(self, use_lib, mod_name, nonlin):
+    def __init__(self, use_lib, mod_name, nonlin, ts, df):
         '''
         class constructor for model class.
 
         This class constructor looks for two arguments:
             use_lib - exp: boolean - whether to leverage dynamics library
             mod_name - exp: string - name of the model (in dynamics library)
+            non_lin - exp: boolean - whether or not to use nonlinear dynamic structure
+            ts - exp: double/float - time sample value
+            df - exp: boolean - discrete time flag modifier
         '''
 
         # Flag for denoting hand-defined dynamics
         self.use_lib = use_lib
         self.mod_name = mod_name
         self.nonlin   = nonlin
+        self.ts = ts
+        self.df = df
+
+        # Declare the 
         if self.use_lib:
             # Perform a look-up in the dynamics library 
             pass
@@ -36,7 +46,12 @@ class model(object):
                 self.f_expr = self._declareFuncf(self.x, self.params)
                 self.g_expr = self._declareFuncg(self.x, self.params)
                 self.cont_model = self.f_expr + self.g_expr*self.u 
-            else: 
+                if self.df: 
+                    self.disc_model = self._discretizeDyn()
+                else: 
+                    # Evaluate the system in a continuous form:
+                    pass
+            else:
                 # This is a linear system, will implement this later
                 pass
 
@@ -92,7 +107,7 @@ class model(object):
         declare all the model dynamics function regularizaiton information using the SymPy notation
         '''
 
-        f_expr = sp.Matrix([[x[0]], [-1*sp.sin(x[0])*params[2]/params[1]]])
+        f_expr = sp.Matrix([[x[1]], [-1*sp.sin(x[0])*params[2]/params[1]]])
  
         return f_expr
 
@@ -104,6 +119,40 @@ class model(object):
         g_expr = sp.Matrix([[0], [1/(params[0]*params[1]**2)]])
 
         return g_expr
+
+    def _discretizeDyn(self):
+        '''
+        use a first order Euler Approximation to discretize the system dynamics
+        '''
+        dis_model = self.x + self.ts*self.cont_model
+        return dis_model
+
+    def evaluate(self, x_sub, u_sub, params_sub):
+        '''
+        evaluate the dynamics with a specific values substituted for the state and the parameters
+        '''
+        if self.df:
+            evaled = self.disc_model.subs(self.x[0], x_sub[0])
+            counter = 0
+            for i in self.x:
+                evaled = evaled.subs(i, x_sub[counter])
+                counter += 1
+            counter = 0
+            for i in self.u:
+                evaled = evaled.subs(i, u_sub[counter])
+                counter += 1
+            counter = 0
+            for i in self.params:
+                evaled = evaled.subs(i, params_sub[counter])
+                counter += 1                
+            return evaled
+        else: 
+            # Evaluate the expression using a continuous time flag
+            evaled = self.cont_model.subs(self.x, x_sub)
+            evaled = evaled.subs(self.u, u_sub)            
+            evaled = evaled.subs(self.params, params_sub)
+            return evaled
+
 
     def forward(self, x):
         '''
@@ -122,7 +171,10 @@ class model(object):
 
 if __name__ == '__main__':
     print("This is the main, do not wear it out!")
-    pen_model = model(0, 'nonlin_pen', 1)
+    pen_model = model(0, 'nonlin_pen', 1, 0.01, 1)
     print(pen_model.mod_name)
     print(pen_model.u)
     print(pen_model.cont_model)
+    print(pen_model.disc_model)
+    evaled = pen_model.evaluate([m.pi, 1], [0.01], [2, 9.81, 9.81])
+    print(evaled)
