7068452f7f6f77f3fd92ad05a5a7a88c472c458d
cnoren <cnoren@andrew.cmu.edu>
2020-06-18 11:01:01 -0400

    Added Measurement Function

--- model/model.py
+++ model/model.py
@@ -4,50 +4,51 @@ import numpy as np
 import sympy as sp
 import math as m
 
-class model(object):
-    def __init__(self, use_lib, mod_name, nonlin, ts, df):
+class Model(object):
+    def __init__(self, use_library, model_name, nonlinear_flag, time_sample, disc_flag):
         '''
         class constructor for model class.
 
         This class constructor looks for two arguments:
-            use_lib - exp: boolean - whether to leverage dynamics library
-            mod_name - exp: string - name of the model (in dynamics library)
-            non_lin - exp: boolean - whether or not to use nonlinear dynamic structure
-            ts - exp: double/float - time sample value
-            df - exp: boolean - discrete time flag modifier
+            use_library - exp: boolean - whether to leverage dynamics library
+            model_name - exp: string - name of the model (in dynamics library)
+            nonlinear_flag - exp: boolean - whether or not to use nonlinear dynamic structure
+            time_sample - exp: double/float - time sample value
+            disc_flag - exp: boolean - discrete time flag modifier
         '''
 
         # Flag for denoting hand-defined dynamics
-        self.use_lib = use_lib
-        self.mod_name = mod_name
-        self.nonlin   = nonlin
-        self.ts = ts
-        self.df = df
+        self.use_library = use_library
+        self.model_name = model_name
+        self.nonlinear_flag   = nonlinear_flag
+        self.time_sample = time_sample
+        self.disc_flag = disc_flag
 
         # Declare the 
-        if self.use_lib:
+        if self.use_library:
             # Perform a look-up in the dynamics library 
             pass
         else: 
-            state_dec = self._declareState()
+            state_dec = self._declare_state()
             self.x = state_dec[0] # This defines the state vector
             self.shape_x = state_dec[1] # This is the shape of the state vector
 
-            cntl_dec = self._declareCntl()
+            cntl_dec = self._declare_cntl()
             self.u = cntl_dec[0] # This defines the state vector
             self.shape_u = cntl_dec[1] # This is the shape of the control vector
-            params_dec = self._declareParams()
+            params_dec = self._declare_params()
             self.params = params_dec[0]
             self.shape_params = params_dec[1]
 
             # Determine if this a nonlinear system or a linear system
-            if self.nonlin:
+            if self.nonlinear_flag:
                 # Declare f(x, params)
-                self.f_expr = self._declareFuncf(self.x, self.params)
-                self.g_expr = self._declareFuncg(self.x, self.params)
+                self.f_expr = self._declare_func_f()
+                self.g_expr = self._declare_func_g()
                 self.cont_model = self.f_expr + self.g_expr*self.u 
-                if self.df: 
-                    self.disc_model = self._discretizeDyn()
+                self.measure_func = self._declare_func_measure()
+                if self.disc_flag: 
+                    self.disc_model = self._discretize_dyn()
                 else: 
                     # Evaluate the system in a continuous form:
                     pass
@@ -56,7 +57,7 @@ class model(object):
                 pass
 
 
-    def _declareState(self):
+    def _declare_state(self):
         '''
         declare model states in SymPy Notation if not using dynamics library
         '''
@@ -71,7 +72,7 @@ class model(object):
         # return the states in aggregation and the shape of the state vector:
         return [x, shape_x]
 
-    def _declareCntl(self):
+    def _declare_cntl(self):
         '''
         declare model control in SymPy Notation if not using dynamics library
         '''
@@ -86,7 +87,7 @@ class model(object):
         # return the controls in aggregation and the shape of the control vector:
         return [u, shape_u]
 
-    def _declareParams(self):
+    def _declare_params(self):
         '''
         declare all model parameters in SymPy Notation if not using dynamics library
         '''
@@ -102,36 +103,42 @@ class model(object):
         return [params, shape_params]
 
 
-    def _declareFuncf(self, x, params):
+    def _declare_func_f(self):
         '''
         declare all the model dynamics function regularizaiton information using the SymPy notation
         '''
 
-        f_expr = sp.Matrix([[x[1]], [-1*sp.sin(x[0])*params[2]/params[1]]])
+        f_expr = sp.Matrix([[self.x[1]], [-1*sp.sin(self.x[0])*self.params[2]/self.params[1]]])
  
         return f_expr
 
-    def _declareFuncg(self, x, params):
+    def _declare_func_g(self):
         '''
         declare all the model dynamics function control weight information using the SymPy notation
         '''
 
-        g_expr = sp.Matrix([[0], [1/(params[0]*params[1]**2)]])
+        g_expr = sp.Matrix([[0], [1/(self.params[0]*self.params[1]**2)]])
 
         return g_expr
 
-    def _discretizeDyn(self):
+    def _declare_func_measure(self):
+        '''
+        declare all measurement outputs for the system
+        '''
+        return sp.eye(self.shape_x[0])*self.x
+
+    def _discretize_dyn(self):
         '''
         use a first order Euler Approximation to discretize the system dynamics
         '''
-        dis_model = self.x + self.ts*self.cont_model
+        dis_model = self.x + self.time_sample*self.cont_model
         return dis_model
 
-    def evaluate(self, x_sub, u_sub, params_sub):
+    def evaluate_dynamics(self, x_sub, u_sub, params_sub):
         '''
         evaluate the dynamics with a specific values substituted for the state and the parameters
         '''
-        if self.df:
+        if self.disc_flag:
             evaled = self.disc_model.subs(self.x[0], x_sub[0])
             counter = 0
             for i in self.x:
@@ -151,8 +158,18 @@ class model(object):
             evaled = self.cont_model.subs(self.x, x_sub)
             evaled = evaled.subs(self.u, u_sub)            
             evaled = evaled.subs(self.params, params_sub)
-            return evaled
+            return [evaled, np.array(evaled).astype(np.float64)]
 
+    def evaluate_measurement(self, x_sub):
+        '''
+        evaluate the measurement function with a specific value substituted fro the state and parameters. Currently assumes full-state feedback
+        '''
+        evaled = self.measure_func.subs(self.x[0], x_sub[0])
+        counter = 0
+        for i in self.x:
+            evaled = evaled.subs(i, x_sub[counter])
+            counter += 1
+        return [evaled, np.array(evaled).astype(np.float64)] 
 
     def forward(self, x):
         '''
@@ -171,10 +188,15 @@ class model(object):
 
 if __name__ == '__main__':
     print("This is the main, do not wear it out!")
-    pen_model = model(0, 'nonlin_pen', 1, 0.01, 1)
-    print(pen_model.mod_name)
+    pen_model = Model(0, 'nonlin_pen', 1, 0.01, 1)
+    print(pen_model.model_name)
     print(pen_model.u)
     print(pen_model.cont_model)
     print(pen_model.disc_model)
-    evaled = pen_model.evaluate([m.pi, 1], [0.01], [2, 9.81, 9.81])
+    print(pen_model.measure_func)
+    evaled = pen_model.evaluate_dynamics([m.pi, 1], [0.01], [2, 9.81, 9.81])
+    print('Value of the state when placed in the dynamics')
     print(evaled)
+    print('Value of the expected measurement equation')
+    evaled_measure = pen_model.evaluate_measurement([m.pi, 1])
+    print(evaled_measure)
