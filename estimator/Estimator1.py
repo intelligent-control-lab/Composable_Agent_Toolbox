f54fb26693837e128fbfcf264aad1592d8a5ebbd
Jaskaran <jaskarag@andrew.cmu.edu>
2020-06-21 14:07:50 -0400

    "fixed EKF code in Estimator1.py, EKF_test.py has standalone EKF code, I'll put UKF in Estimator1.py"

--- estimator/Estimator1.py
+++ estimator/Estimator1.py
@@ -9,12 +9,10 @@ class Estimator_Base():
 	estimator_name = ''
 	estimator_type = '' # EKF,UKF,RLS etc.
 
-	
 	@abstractmethod
 	def __init__(self):
 		pass
 	
-
 	@abstractmethod
 	def estimate(self):
 		'''
@@ -50,7 +48,7 @@ class Zero_Estimator(Estimator_Base):
 	def reset(self):
 		return True
 
-	def estimate(self, dt, x, goal_x, est_params):
+	def estimate(self, x,covariance,z):
 		return np.zeros(self.model.x_shape)
 
 
@@ -64,45 +62,51 @@ class Estimator(Estimator_Base):
     E.g. EKF Estimator specs:
     {"Extended Kalman filter": "EKF", "params": {"Rww": [1,2], "Rvv": [0, 0]}}
     ''' 
-
 	def __init__(self,spec,model):
 		self.spec  = spec
 		self.model = model
 
 		if 'EKF' in self.spec:
-			self.EKF = globals()[self.spec['EKF']](self.spec['params'],self.model)
+			self.EKF = globals()[self.spec['EKF']](self.spec['params'],self.model) 
+			# this line will take input from spec, check reflection in python. it takes input from spec and 
+			# translates a string used in spec and creates a class object deepnds on string
+			# example : If I define a feedback controller, with PID, when I change PID to something else like adatpive controller
+			# it will automatically create an ARC class object.
+
 		else:
 			self.EKF = Zero_Estimator(self.spec,self.model)
 
 
-
+# put underscore to internal variables
 class Extended_Kalman_Filter(Estimator_Base):
 
 	def __init__(self,estimator_params,model):
-		self.name = 'EKF'
-		self.params = estimator_params
-		self.model  = model
-		self.Rww    = estimator_params['Rww']
-		self.Rvv    = estimator_params['Rvv']
+		self.name 				 = 'EKF'
+		self.params 			 = estimator_params
+		self.model  			 = model
+		self._Rww    			 = estimator_params['Rww']
+		self._Rvv    			 = estimator_params['Rvv']
 
 	def param_tuning(self,params):
-		self.Rww = params['Rww']
-		self.Rvv = params['Rvv']
+		self._Rww 				 = params['Rww']
+		self._Rvv 				 = params['Rvv']
 		return True
 
 	def estimate(self,posterior_pos,posterior_state_cov,z_meas):
-		priori_pos               = robot_model.forward_propagate_dynamics(posterior_state)      				; # need to call dynamics of the paritcular robot model
-		A                        = robot_model.linearization_forward_dynamics(posterior_state)  				; # need to call dynamics of the paritcular robot model
-		priori_cov               = np.matmul(np.matmul(A,posterior_state_cov),np.transpose(A)) + self.Rww      	;   
+		# check Chase's code, see which method gives next state
+		priori_pos               = self.forward_propagate_dynamics(posterior_pos)      							; # need to call dynamics of the paritcular robot model
+		A                        = self.linearization_forward_dynamics(posterior_pos)  							; # need to call dynamics of the paritcular robot model
+		priori_cov               = np.matmul(np.matmul(A,posterior_state_cov),np.transpose(A)) + self._Rww      ;   
 		n                        = len(posterior_pos)                                           				;
 		I                        = np.identity(n)                                               				;
-		H                        = robot_model.linearization_measurement_function(priori_pos)   				; # need to call dynamics of the paritcular robot model
-		innovation               = z_meas - robot_model.measurement_function(priori_pos)        				; # need to call dynamics of the paritcular robot model
-		S                        = np.matmul(np.matmul(H,priori_cov),np.transpose(H)) + self.Rvv  				;
+		H                        = self.linearization_measurement_function(priori_pos)   						; # need to call dynamics of the paritcular robot model
+		innovation               = z_meas - self.measurement_function(priori_pos)        						; # need to call dynamics of the paritcular robot model
+		S                        = np.matmul(np.matmul(H,priori_cov),np.transpose(H)) + self._Rvv  				;
 		Sinv                     = np.linalg.inv(S)																;
-		K                        = np.matmul(priori_cov,np.transpose(H))*Sinv                   				;
+		K                        = np.matmul(np.matmul(priori_cov,np.transpose(H)),Sinv)                  		;
 		next_posterior_state     = priori_pos + np.matmul(K,innovation)                         				;
 		next_posterior_state_cov = np.matmul(I-(np.matmul(K,H)),priori_cov)                     				;
+
 		return next_posterior_state,next_posterior_state_cov
 
 	def reset(self):
