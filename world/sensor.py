b359eccc958a81177f03e3b2db1bc1bb02e96dd4
Tianhao <phi.wth@gmail.com>
2020-06-23 00:31:17 -0400

    add RadarSensor, add alias attr to sensors, change the return form to dict

--- world/sensor.py
+++ world/sensor.py
@@ -1,11 +1,40 @@
 import numpy as np
+from abc import ABC, abstractmethod
 
-class PVSensor(object):
-    def __init__(self, target, spec):
-        self.target = target
-    def _noise(self, x):
-        return x + (np.random.random_sample(x.shape) * 0.05 - 0.05)
+class Sensor(ABC):
+ 
+    def __init__(self, agent, all_agents, spec):
+        self.agent = agent
+        self.all_agents = all_agents
+        self.alias = spec["alias"]
+
+    def _gaussian_noise(self, x, variance):
+        return x + (np.random.random_sample(x.shape) * variance - variance/2)
+
+    @abstractmethod
     def measure(self):
-        pos = self.target.pos
-        vel = self.target.vel
-        return self._noise(pos), self._noise(vel)
+        pass
+
+class PVSensor(Sensor):
+    def __init__(self, agent, all_agents, spec):
+        super().__init__(agent, all_agents, spec)
+        self.noise_var = spec['noise_var']
+
+    def measure(self):
+        ret = {
+            "pos": self._gaussian_noise(self.agent.pos, self.noise_var),
+            "vel": self._gaussian_noise(self.agent.vel, self.noise_var)
+        }
+        return ret
+
+class RadarSensor(Sensor):
+    def __init__(self, agent, all_agents, spec):
+        super().__init__(agent, all_agents, spec)
+        self.noise_var = spec['noise_var']
+
+    def measure(self):
+        distances = {}
+        for agent in self.all_agents:
+            if agent != self.agent:
+                distances[agent.name] = self._gaussian_noise(agent.pos - self.agent.pos, self.noise_var)
+        return distances
