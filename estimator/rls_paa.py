7fd1a13975922c05e08c6b81875b6d8e7a830586
Jaskaran <jaskarag@andrew.cmu.edu>
2020-08-29 13:25:55 -0400

    just pushing rls_paa.py  Please enter the commit message for your changes. Lines starting

--- estimator/rls_paa.py
+++ estimator/rls_paa.py
@@ -23,7 +23,6 @@ class RLSPredictor(object):
         self.pred_state          = None
         self.cache               = {}
 
-
     def predict(self, u,sensor_data):
         other_agent_state = np.zeros((4,1))
 
@@ -35,7 +34,7 @@ class RLSPredictor(object):
         for name in sensor_data["communication_sensor"].keys():
             st = sensor_data["communication_sensor"][name]
             for things in st.keys():
-                other_agent_state = np.ravel(st[things])
+                other_agent_state   = np.ravel(st[things])
 
         z_meas                      = np.vstack(np.ravel(sensor_data["state_sensor"]["state"]))
         other_agent_measurement     = np.vstack(np.ravel(other_agent_state))
@@ -46,28 +45,24 @@ class RLSPredictor(object):
         psi                         = np.vstack(np.ravel(psi))
         other_agent_priori_state    = np.vstack(np.ravel(np.matmul(self.C,psi)))
         other_agent_posterior_state = (1-self.alpha)*other_agent_priori_state + (self.alpha*other_agent_measurement)
-
         self.C                      = self.C + np.matmul(self.epsilon,np.matmul(np.transpose(psi),self.F))
         self.epsilon                = other_agent_posterior_state - other_agent_priori_state
         expression                  = self.lamb + (np.matmul(np.transpose(psi),np.matmul(self.F,psi)))
         self.F                      = (self.F - (np.matmul(self.F,np.matmul(np.matmul(psi,np.transpose(psi)),self.F)))/expression)/self.lamb
-
-        A                        = np.array([[1.0,0.0,self.dt,0.0],[0.0,1.0,0.0,self.dt],[ -(self.dt*self.kp)/2,0.0, 1.0 - (self.dt*self.kv)/2,0.0],[0.0,-(self.dt*self.kp)/2,0.0, 1.0 - (self.dt*self.kv)/2]])
-        priori_cov               = np.matmul(np.matmul(A,self.posterior_state_cov),np.transpose(A)) + self._Rww      ;   
-        n                        = len(self.posterior_state)                                                         ;
-        I                        = np.identity(n)                                                               ;
-        H                        = I 
+        A                           = np.array([[1.0,0.0,self.dt,0.0],[0.0,1.0,0.0,self.dt],[ -(self.dt*self.kp)/2,0.0, 1.0 - (self.dt*self.kv)/2,0.0],[0.0,-(self.dt*self.kp)/2,0.0, 1.0 - (self.dt*self.kv)/2]])
+        priori_cov                  = np.matmul(np.matmul(A,self.posterior_state_cov),np.transpose(A)) + self._Rww      ;   
+        n                           = len(self.posterior_state)                                                         ;
+        I                           = np.identity(n)                                                               ;
+        H                           = I 
         # H                        = self.linearization_measurement_function(priori_state)                        ; # need to call dynamics of the paritcular robot model
-        
-        innovation               = z_meas - (ego_priori_state)
-
-        S                        = np.matmul(np.matmul(H,priori_cov),np.transpose(H)) + self._Rvv               ;
-        Sinv                     = np.linalg.inv(S)                                                             ;
-        K                        = np.matmul(np.matmul(priori_cov,np.transpose(H)),Sinv)                        ;
-        self.posterior_state     = ego_priori_state + np.matmul(K,innovation);                                       
-        self.posterior_state_cov = np.matmul(I-(np.matmul(K,H)),priori_cov)                                     ;
-        est_param["ego_state_est"]   = np.vstack(np.ravel(self.posterior_state))
-        est_param["other_state_est"] = np.vstack(np.ravel(other_agent_posterior_state))
+        innovation                  = z_meas - (ego_priori_state)
+        S                           = np.matmul(np.matmul(H,priori_cov),np.transpose(H)) + self._Rvv               ;
+        Sinv                        = np.linalg.inv(S)                                                             ;
+        K                           = np.matmul(np.matmul(priori_cov,np.transpose(H)),Sinv)                        ;
+        self.posterior_state        = ego_priori_state + np.matmul(K,innovation);                                       
+        self.posterior_state_cov    = np.matmul(I-(np.matmul(K,H)),priori_cov)                                     ;
+        est_param["ego_state_est"]  = np.vstack(np.ravel(self.posterior_state))
+        est_param["other_state_est"]= np.vstack(np.ravel(other_agent_posterior_state))
 
         return est_data, est_param
 
