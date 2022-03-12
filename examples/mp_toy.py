307af02ea65402c82827461cffd8b4a381402147
Anirudh Chari <anirudh.chari05@gmail.com>
2022-03-12 09:52:40 -0600

    started multiprocessing agent/env

---
+++ examples/mp_toy.py
@@ -0,0 +1,50 @@
+import sys
+from os.path import abspath, join, dirname
+
+from sympy import init_session
+sys.path.insert(0, join(abspath(dirname(__file__)), '../'))
+import numpy as np
+import evaluator, agent, env
+import time, yaml
+import progressbar
+import multiprocessing
+
+if __name__ == "__main__":
+
+    with open('examples/configs/flat_evade_agent_1.yaml', 'r') as infile:
+        agent_module_spec = yaml.load(infile, Loader=yaml.SafeLoader)
+    agent1 = agent.MPAgent(agent_module_spec)
+    agents = [agent1]
+
+    # The environment specs, including specs for the phsical agent model,
+    # physics engine scenario, rendering options, etc.
+    with open('examples/configs/flat_evade_env.yaml', 'r') as infile:
+        env_spec = yaml.load(infile, Loader=yaml.SafeLoader)
+    evaluator = evaluator.Evaluator(agent_module_spec, env_spec)
+    env = env.MPEnv(env_spec, agents)
+
+    manager = multiprocessing.Manager()
+
+    mgr_record = manager.Queue()
+    mgr_sensor_data = manager.dict()
+    init_env_info, init_sensor_data = env.reset()
+    mgr_record.put((init_env_info, init_sensor_data))
+    mgr_sensor_data.update(init_sensor_data)
+
+    mgr_actions = manager.dict()
+    init_actions = agent1.init_action(init_sensor_data)
+    mgr_actions[agent1.name] = init_actions
+
+    lock = manager.Lock()
+    iters = 500
+
+    agent_process = multiprocessing.Process(target=agent1.action, args=(mgr_actions, mgr_sensor_data, lock, iters))
+    env_process = multiprocessing.Process(target=env.step, args=(mgr_actions, mgr_sensor_data, mgr_record, lock, iters))
+
+    agent_process.start()
+    env_process.start()
+
+    agent_process.join()
+    env_process.join()
+    
+    evaluator.evaluate(mgr_record)
