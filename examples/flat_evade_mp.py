13d41a75316ac28cd0b88396983d5e525f1e9763
Anirudh Chari <anirudh.chari05@gmail.com>
2022-04-17 21:30:54 -0500

    add flat_evade_env_mp.yaml

--- examples/flat_evade_mp.py
+++ examples/flat_evade_mp.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 
     # The environment specs, including specs for the phsical agent model,
     # physics engine scenario, rendering options, etc.
-    with open('configs/flat_evade_env.yaml', 'r') as infile:
+    with open('configs/flat_evade_env_mp.yaml', 'r') as infile:
         env_spec = yaml.load(infile, Loader=yaml.SafeLoader)
     evaluator = evaluator.Evaluator(agent_module_spec, env_spec)
     env = env.FlatEvadeEnvMP(env_spec, agents)
