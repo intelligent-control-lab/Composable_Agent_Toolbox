f28bd4896763fd5d5d00643cf0f9bcbeb48b213b
anirudhc1229 <anirudh.chari05@gmail.com>
2022-11-23 21:09:00 -0600

    add runtime testing

--- examples/sim_mas_seq.py
+++ examples/sim_mas_seq.py
@@ -1,4 +1,5 @@
 import importlib
+import math
 import sys
 from os.path import abspath, join, dirname
 sys.path.insert(0, join(abspath(dirname(__file__)), '../'))
@@ -31,7 +32,7 @@ def visualize(stg):
     plt.show()
     return
 
-if __name__ == '__main__':
+def main():
 
     # config.yaml would contain yaml filenames of user's desired agents/env
     with open('configs/config.yaml', 'r') as infile:
@@ -81,6 +82,7 @@ if __name__ == '__main__':
     a_max = [10 for ag in agents]
     gamma = [1 for ag in agents]
     priority = [1 for ag in agents]
+    # priority[1] = 100
     obs_paths = [[np.array(ob.path[0][:2])] for ob in obs]
     obs_dt = np.array([ob.dt for ob in obs])
     stg = SpaceTimeGrid(paths, r, ag_dt, a_max, gamma, priority, obs_paths, obs_dt)
@@ -92,17 +94,6 @@ if __name__ == '__main__':
             path = ob.path.copy()
             path.append(waypoint)
             ob.set_path(path)
-
-    # # plot obstacle paths
-    # fig = plt.figure()
-    # ax = fig.add_subplot(projection='3d')
-    # colors = 'brgcmk'
-    # for i, p in enumerate(stg.obs_paths):
-    #     x = [s[0] for s in p]
-    #     y = [s[1] for s in p]
-    #     t = [s[2] for s in p]
-    #     ax.scatter(x, y, t, color=colors[i])
-    # plt.show()
 
     print("Simulation progress:")
     for it in progressbar.progressbar(range(iters)):
@@ -120,23 +111,41 @@ if __name__ == '__main__':
                 path = stg.get_path(j)
                 a.set_path(path)
         
-    visualize(stg)
-
     print(f"OPT NUM: {stg.opt_num}")
     print(f"TREE TIME: {stg.tree_time}")
     print(f"AVG OPTS PER RES {stg.opt_num / stg.res_num}")
     print(f"RESOLVE TIME: {stg.resolve_time - stg.tree_time}")
 
-    makespan = stg.paths[0][-1][2]
+    T = stg.paths[0][-1][2]
     for i in range(1, len(stg.paths)):
-        makespan = max(makespan, stg.paths[i][-1][2])
-    print(f"MAKESPAN: {makespan}")
+        T = max(T, stg.paths[i][-1][2])
+    print(f"T: {T}")
 
-    tot_dist = 0
+    D = 0
     for p in stg.paths:
         for i in range(len(p) - 1):
-            tot_dist += np.linalg.norm(p[i + 1] - p[i])
-    print(f"TOTAL DISTANCE: {tot_dist}")
+            D += np.linalg.norm(p[i + 1] - p[i])
+    print(f"D: {D}")
+
+    T_star = 0
+    for i, p in enumerate(stg.paths):
+        d = np.linalg.norm(p[-1] - p[0])
+        a = a_max[i]
+        t = math.sqrt(2 * a * d) / a
+        T_star = max(T_star, t)
+    print(f"T*: {T_star}")
+
+    D_star = 0
+    for p in stg.paths:
+        D_star += np.linalg.norm(p[-1] - p[0])
+    print(f"D*: {D_star}")
+
+    print(f"T SUBOPT: {T / T_star}")
+    print(f"D SUBOPT: {D / D_star}")
+    subopt_ratio = (T / T_star + D / D_star) / 2
+    print(f"SUBOPTIMALITY RATIO: {subopt_ratio}")
+
+    visualize(stg)
 
     min_clear = np.inf
     for i, p1 in enumerate(stg.paths + stg.obs_paths):
@@ -146,14 +155,35 @@ if __name__ == '__main__':
                     min_clear = min(min_clear, np.linalg.norm(s1 - s2) / stg.alpha)
     print(f"MIN CLEARANCE FINAL: {min_clear}")
 
-    dt, env_info, measurement_groups = env.reset()
-    for i in range(iters):
-        actions = {}
-        for ag in agents + obs:
-            # an action is dictionary which must contain a key "control"
-            actions[ag.name] = ag.action(dt, measurement_groups[ag.name])
-            #sensor data is grouped by agent
-            print(f"{ag.name} : {actions[ag.name]['control']} : {actions[ag.name]['broadcast']['next_point']}")
-        dt, env_info, measurement_groups = env.step(actions, debug_modes, render=render)
+    # dt, env_info, measurement_groups = env.reset()
+    # for i in range(iters):
+    #     actions = {}
+    #     for ag in agents + obs:
+    #         # an action is dictionary which must contain a key "control"
+    #         actions[ag.name] = ag.action(dt, measurement_groups[ag.name])
+    #         #sensor data is grouped by agent
+    #         print(f"{ag.name} : {actions[ag.name]['control']} : {actions[ag.name]['broadcast']['next_point']}")
+    #     dt, env_info, measurement_groups = env.step(actions, debug_modes, render=render)
 
     # evaluator.evaluate(record)
+
+    # plt.axis()
+    # colors = 'brgcmk'
+    # points = None
+    # for i in range(iters):
+    #     plt.cla()
+    #     for j, ag in enumerate(agents + obs):
+    #         plt.xlim([0, 25])
+    #         plt.ylim([0, 25])
+    #         idx = min(i, len(ag.path) - 1)
+    #         points = plt.scatter(ag.path[idx][0], ag.path[idx][1], color=colors[j])
+    #         plt.pause(0.001)
+    # plt.show()
+
+    return stg.resolve_time - stg.tree_time
+
+if __name__ == '__main__':
+    tot = 0
+    for i in range(20):
+        tot += main()
+    print(f"AVERAGE OVER 20 TRIALS: {tot / 20}")
