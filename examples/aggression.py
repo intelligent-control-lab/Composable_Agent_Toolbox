064993c41eb97131e36e751106d0ca5ddc107cf6
Anirudh Chari <anirudh.chari05@gmail.com>
2023-07-04 22:57:17 -0500

    simple case working

--- examples/aggression.py
+++ examples/aggression.py
@@ -1,3 +1,4 @@
+import math
 import random
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
@@ -5,8 +6,8 @@ from matplotlib.patches import Rectangle
 nH = 1
 nR = 1
 dt = 0.01
-T = 10000
-L = 1
+t_max = 10000
+L = 5
 
 xH = []
 vH = []
@@ -15,9 +16,16 @@ xR = []
 vR = []
 lR = []
 
-s_min = L
-vH_max = [30]
-dvH_th = [10]
+s_min = 2*L
+a_max = 1.5
+vR_max = 25
+
+s0 = L / 4
+v0 = 30
+dvH_th = 10
+T = 0.01 # 1.3
+a = 1.8
+b = 3.1
 
 alpha = 0
 beta = 0
@@ -36,12 +44,12 @@ def plot(fref):
 
     for i in range(nH):
         ax.add_patch(Rectangle(
-            (lH[i] - L/4, xH[i] - L/2), 
+            (lH[i]*L - L/4, xH[i] - L/2), 
             L/2, L, 
             facecolor='blue'))
     for i in range(nR):
         ax.add_patch(Rectangle(
-            (lR[i] - L/4, xR[i] - L/2), 
+            (lR[i]*L - L/4, xR[i] - L/2), 
             L/2, L, 
             facecolor='red'))
 
@@ -52,8 +60,8 @@ def apply_control(u):
     aH, dH = f_theta()
     aR, dR = u
 
-    f_theta_last = [(aH, dH)]
-    u_last = [(aR, dR)]
+    print(f"HUMAN ACC: {aH}\tVEL: {vH}")
+    print(f"ROBOT ACC: {aR}\tVEL: {vR}")
 
     for i in range(nH):
         vH[i] += aH[i] * dt
@@ -62,14 +70,23 @@ def apply_control(u):
             lH[i] + round(dH[i])))
         
     for i in range(nR):
+        aR[i] = min(a_max, aR[i])
         vR[i] += aR[i] * dt
+        vR[i] = min(vR_max, vR[i])
         xR[i] += vR[i] * dt
         lR[i] = max(-1, min(1, 
             lR[i] + round(dR[i])))
 
 def f_theta():
-    return ([0.8 for _ in range(nH)], 
-            [random.gauss(0, 0.2) for _ in range(nH)])
+    aH = []
+    for i in range(nH):
+        s_star = s0 + vH[i] * T + (vH[i] * (vH[i] - vR[0])) / (2 * math.sqrt(a * b))
+        print(s0, vH[i], T, vR[0], a, b)
+        print(s_star)
+        aH.append(a * (1 - (vH[i] / v0)**4 
+                       - (s_star / (xR[0] - xH[i] - L))**2))
+    lH = [v0 - vR[0] >= dvH_th for _ in range(nH)]
+    return (aH, lH)
 
 def hF():
     return xR[0] - xH[0] - L - s_min
@@ -84,7 +101,7 @@ def get_alphabeta():
     return (p1 + p2, p1 * p2)
 
 def hL():
-    return vH_max[0] - vR[0] - dvH_th[0]
+    return v0 - vR[0] - dvH_th
 def hL_dot():
     return vR[0] - vH[0]
 def get_p3():
@@ -94,6 +111,7 @@ def compute_u():
     phiF = f_theta()[0][0] - alpha * hF_dot() - beta * hF()
     uF = max(0, phiF)
     uL = min(0, p3 * hL())
+    print(f'PHIF: {phiF}\tP3HL: {p3 * hL()}')
     u_star = uF if uF**2 < uL**2 else uL
     return ([u_star for _ in range(nR)], 
             [0 for _ in range(nR)])
@@ -101,7 +119,7 @@ def compute_u():
 if __name__ == '__main__':
 
     for i in range(nH):
-        xH.append(-3*L)
+        xH.append(-2*L)
         vH.append(25)
         lH.append(0)
     for i in range(nR):
@@ -112,7 +130,7 @@ if __name__ == '__main__':
     alpha, beta = get_alphabeta()
     p3 = get_p3()
 
-    for t in range(T):
+    for t in range(t_max):
         u = compute_u()
         apply_control(u)
-        plot(xH[0])
+        plot(xR[0])
