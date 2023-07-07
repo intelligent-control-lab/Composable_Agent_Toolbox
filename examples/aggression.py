40d1cdaaf0d64c70c59ee56bccc4f121436cfe7d
Anirudh Chari <anirudh.chari05@gmail.com>
2023-07-07 15:11:31 -0500

    add get_following method

--- examples/aggression.py
+++ examples/aggression.py
@@ -17,12 +17,12 @@ xR = []
 vR = []
 lR = []
 
-s_min = 1.2*L
+s_min = 1.4*L
 a_max = 1.5
 vR_max = 35
 
-s0 = 0.25*L
-v0 = 30
+s0 = 0.3*L
+v0 = 35
 dvH_th = 10
 T = 0.01 # 1.3
 a = 1.8
@@ -73,13 +73,19 @@ def plot(fref):
 def safe(): 
     return xR[0] - xH[0] - L >= s_min or lH[0] != lR[0]
 
+def get_following(i):
+    f = -1
+    for j in range(nR):
+        if lH[i] != lR[j] or xH[i] > xR[j]:
+            continue
+        if f == -1 or xR[j] < xR[f]:
+            f = j
+    return f
+
 def apply_control(u):
 
     aH, dH = f_theta()
     aR, dR = u
-
-    print(f"HUMAN ACC: {aH}\tVEL: {vH}")
-    print(f"ROBOT ACC: {aR}\tVEL: {vR}")
 
     for i in range(nH):
         vH[i] += aH[i] * dt
@@ -97,14 +103,16 @@ def apply_control(u):
 
 def f_theta():
     aH = []
+    dH = []
     for i in range(nH):
-        s_star = s0 + vH[i] * T + (vH[i] * (vH[i] - vR[0])) / (2 * math.sqrt(a * b))
-        print(s0, vH[i], T, vR[0], a, b)
-        print(s_star)
-        aH.append(a * (1 - (vH[i] / v0)**4 
-                       - (s_star / (xR[0] - xH[i] - L))**2))
-    lH = [v0 - vR[0] >= dvH_th for _ in range(nH)]
-    return (aH, lH)
+        f = get_following(i)
+        acc = a * (1 - (vH[i] / v0)**4)
+        if f != -1:
+            s_star = s0 + vH[i] * T + (vH[i] * (vH[i] - vR[f])) / (2 * math.sqrt(a * b))
+            acc += -a * (s_star / (xR[f] - xH[i] - L))**2
+        aH.append(acc)
+        dH.append(0 if f == -1 else v0 - vR[f] >= dvH_th)
+    return (aH, dH)
 
 def hF():
     return xR[0] - xH[0] - L - s_min
@@ -113,9 +121,9 @@ def hF_dot():
 def hF_ddot():
     return compute_u()[0][0] - f_theta()[0][0]
 def get_alphabeta():
-    p1 = max(0.1, -hF_dot() / hF())
-    p2 = max(0.1, -(hF_ddot() + p1 * hF_dot()) 
-                    / (hF_dot() + p1 * hF()))
+    p1 = max(0, -hF_dot() / hF()) + 0.1
+    p2 = max(0, -(hF_ddot() + p1 * hF_dot()) 
+                    / (hF_dot() + p1 * hF())) + 0.1
     return (p1 + p2, p1 * p2)
 
 def hL():
@@ -123,7 +131,7 @@ def hL():
 def hL_dot():
     return vR[0] - vH[0]
 def get_p3():
-    return max(0.1, -hL_dot() / hL())
+    return max(0, -hL_dot() / hL()) + 0.1
 
 def compute_u():
     phiF = f_theta()[0][0] - alpha * hF_dot() - beta * hF()
@@ -131,7 +139,6 @@ def compute_u():
         phiF = np.inf
     uF = max(0, phiF)
     uL = min(0, p3 * hL())
-    print(f'PHIF: {phiF}\tP3HL: {p3 * hL()}')
     u_star = uF if uF**2 < uL**2 else uL
     return ([u_star for _ in range(nR)], 
             [0 for _ in range(nR)])
@@ -140,11 +147,11 @@ if __name__ == '__main__':
 
     for i in range(nH):
         xH.append(-2*L)
-        vH.append(29)
+        vH.append(30)
         lH.append(0)
     for i in range(nR):
         xR.append(0)
-        vR.append(29)
+        vR.append(26)
         lR.append(0)
 
     alpha, beta = get_alphabeta()
