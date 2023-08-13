bc9f61660873d41312a03538752c601fc1f8d872
Anirudh Chari <anirudh.chari05@gmail.com>
2023-08-13 18:20:57 -0500

    change method names

--- examples/CBF_hV.py
+++ examples/CBF_hV.py
@@ -5,18 +5,18 @@ class CBF_hV:
         self.idm = idm
         self.alpha = self._alpha(x_init)
 
-    def hV(self, x):
+    def h(self, x):
         return self.v_max - x['vH'][0]
-    def hV_dot(self, x):
+    def h_dot(self, x):
         return -x['aH'][0]
     
     def ub(self, x):
-        return (self.alpha[1] * self.hV_dot(x) + self.alpha[0] * self.hV(x) 
+        return (self.alpha[1] * self.h_dot(x) + self.alpha[0] * self.h(x) 
           - self.idm.lamb(x['pH'][0], x['pR'][0], x['vH'][0], x['vR'][0], x['aH'][0])) \
     / self.idm.df_dvR(x['pH'][0], x['pR'][0], x['vH'][0], x['vR'][0])
     
     def _alpha(self, x):
-        c1 = max(0, -self.hV_dot(x) / self.hV(x)) + self.c_min
-        c2 = max(0, -(0 + c1 * self.hV_dot(x)) 
-                / (self.hV_dot(x) + c1 * self.hV(x))) + self.c_min
+        c1 = max(0, -self.h_dot(x) / self.h(x)) + self.c_min
+        c2 = max(0, -(0 + c1 * self.h_dot(x)) 
+                / (self.h_dot(x) + c1 * self.h(x))) + self.c_min
         return [c1 + c2, c1 * c2]
