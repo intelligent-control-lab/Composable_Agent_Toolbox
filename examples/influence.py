86f6a36b4356ef3496d49b0c78beecbb11e5f592
Anirudh Chari <anirudh.chari05@gmail.com>
2023-12-04 19:44:18 -0600

    clearn up code

--- examples/influence.py
+++ examples/influence.py
@@ -27,18 +27,17 @@ x = {'pH': [], 'vH': [], 'aH': [], 'lH': [], 'dH': [],
      'pR': [], 'vR': [], 'aR': [], 'lR': [], 
      'pB': [], 'vB': [], 'lB': []}
 
-c_min = 3.25 # 0.8 and 1.2 works too, 2 is really nice, 3.5 best so far
+c_min = 3.25
 
 s_min = 10.0001 - L
 a_max = 2.0
 b_max = 4.0
-# vR_max = [34.5, 38]
 vR_max = [35, 35]
 
 s0 = 0.3*L
 v0 = 35
 vH_th = 10
-T = 0.01 # 1.3
+T = 0.01
 a = 1.8
 b = 3.1
 idm = IDM(s0, v0, T, a, b, L)
@@ -55,7 +54,7 @@ hB = None
 
 fig, ax = plt.subplots()
 
-def vis(pov):
+def vis(pov): # simulation visualizer
     
     ax.cla()
     ax.axis([-4*L, 4*L, pov - 4*L, pov + 4*L])
@@ -64,7 +63,6 @@ def vis(pov):
     ax.axvline(x=-3*L/2, color="black", linestyle="-")
     ax.axvline(x=3*L/2, color="black", linestyle="-")
 
-    # ax.text(-4*L, pov - 4*L, "Safe: " + str(safe()))
     ax.text(-4*L, pov + 4*L, "t = " + str(round(t, 2)))
     for i in range(m):
         ax.text(x['lH'][i]*L - L/4, x['pH'][i], str(round(x['vH'][i], 1)))
@@ -89,42 +87,16 @@ def vis(pov):
             L/2, L, 
             facecolor='green'))
 
-    # ax.add_patch(Rectangle(
-    #         (x['lR'][i]*L - L/4, x['pR'][i] - L/2 - s_min), 
-    #         L/2, s_min,
-    #         facecolor=(1,1,0,0.5)))
-
-    # if x['lH'][0] != -1:
-    #     # ax.add_patch(Rectangle(
-    #     #         (-1*L - L/4, x['pH'][0] - dist_min / 2), 
-    #     #         L/2, dist_min,
-    #     #         facecolor=(1,1,0,0.5)))
-    #     ax.add_patch(Rectangle(
-    #         (x['lB'][i]*L - L/4, x['pB'][i] - L/2 - dist_min), 
-    #         L/2, dist_min,
-    #         facecolor=(1,1,0,0.5)))
-
     plt.pause(0.001)
 
     if round(t, 2) % 2.5 == 0:
         plt.savefig(f'cbf_31_vis-t{round(t, 2)}.png')
 
-def plot():
+def plot(): # pos, vel, acc vs time plots
 
     global x_all
-    # x_all = x_all[10:]
 
     xs = [s[0] for s in x_all]
-
-    # fig0, ax0 = plt.subplots()
-    # y_pH = [s[1]['pH'][0] for s in x_all]
-    # y_pR = [s[1]['pR'][0] for s in x_all]
-    # y = [r - h for h, r in zip(y_pH, y_pR)]
-    # ax0.plot(xs, y, label='dist')
-    # # plt.axhline(y=s_min+L, color='black', linestyle='--')
-    # # ax0.plot(xs, y_pH, label='pH', c='b')
-    # # ax0.plot(xs, y_pR, label='pR', c='r')
-    # ax0.legend(loc='upper right')
 
     fig0, ax0 = plt.subplots()
     y_pR = [s[1]['pR'][0] for s in x_all]
@@ -175,28 +147,10 @@ def plot():
     y_aH1 = [s[1]['aH'][0] for s in x_all]
     y_aR2 = [s[1]['aR'][1] for s in x_all]
     y_aH2 = [s[1]['aH'][1] for s in x_all]
-    # ax3.plot(xs, y_aR1, label='aR1')
     ax3.plot(xs, y_aH1, label='aH1')
-    # ax3.plot(xs, y_aR2, label='aR2')
     ax3.plot(xs, y_aH2, label='aH2')
     plt.axvline(x=t_switch, color='gray', linestyle='--')
     ax3.legend(loc='lower left')
-
-    # fig1, ax1 = plt.subplots()
-    # y = [s[1]['vH'][0] for s in x_all]
-    # ax1.plot(xs, y, label='vH')
-    # plt.axhline(y=v_max, color='black', linestyle='--')
-    # ax1.legend(loc='upper right')
-
-    # fig2, ax2 = plt.subplots()
-    # y_vH = [s[1]['vH'][0] for s in x_all]
-    # y_vR = [s[1]['vR'][0] for s in x_all]
-    # y_vB = [s[1]['vB'][0] for s in x_all]
-    # ax2.plot(xs, y_vH, label='vH', c='b')
-    # ax2.plot(xs, y_vR, label='vR', c='r')
-    # ax2.plot(xs, y_vB, label='vB', c='g')
-    # plt.axvline(x=t_switch, color='gray', linestyle='--')
-    # ax2.legend(loc='lower right')
 
     fig4, ax4 = plt.subplots()
     y_u1 = [s[1]['aR'][0] for s in x_all]
@@ -214,21 +168,13 @@ def plot():
     plt.axvline(x=t_switch, color='gray', linestyle='--')
     plt.axhline(y=3.97, color='black', linestyle='--')
     ax5.legend(loc='lower right')
-
-    # fig4, ax4 = plt.subplots()
-    # y_aH = [s[1]['aH'][0] for s in x_all]
-    # y_aR = [s[1]['aR'][0] for s in x_all]
-    # ax4.plot(xs, y_aH, label='aH', c='b')
-    # ax4.plot(xs, y_aR, label='aR', c='r')
-    # plt.axvline(x=t_switch, color='gray', linestyle='--')
-    # ax4.legend(loc='upper right')
-
+     
     plt.show()
 
-def safe(): 
+def safe(): # whether switching lanes is safe
     return x['pR'][0] - x['pH'][0] - L >= s_min or x['lH'][0] != x['lR'][0]
 
-def get_following(i, l):
+def get_following(i, l): # pos, vel of car followed by car i in lane l
     fol = -1
     for j in range(m):
         if j == i or x['lH'][j] != l or x['pH'][i] > x['pH'][j]:
@@ -253,7 +199,7 @@ def get_following(i, l):
         return x['pB'][fol], x['vB'][fol]
     return None
 
-def is_occupied(p, l):
+def is_occupied(p, l): # whether pos p in lane l is occupied
     for i in range(m):
         if x['lH'][i] == l and abs(p - x['pH'][i]) < dist_min + L:
             return True
@@ -265,7 +211,7 @@ def is_occupied(p, l):
             return True
     return False
 
-def apply_control(u):
+def apply_control(u): # apply controls u to robot cars
 
     aH, dH = f()
     aR, dR = u
@@ -279,8 +225,6 @@ def apply_control(u):
         
     for i in range(n):
         aR[i] = min(a_max, max(-b_max, aR[i]))
-        # if i == 1 and t >= 2.38 and t <= 15.6:
-        #     aR[i] = 2 / 1.322
         x['vR'][i] += aR[i] * dt
         x['vR'][i] = min(vR_max[i], x['vR'][i])
         x['pR'][i] += x['vR'][i] * dt
@@ -294,7 +238,7 @@ def apply_control(u):
     x['aR'] = aR
     x_all.append((t, copy.deepcopy(x)))
 
-def f():
+def f(): # compute human car controls
 
     aH = []
     dH = []
@@ -309,34 +253,35 @@ def f():
             vel_new = v0 if fol_new is None else fol_new[1]
             incentive = vel_new - fol[1] >= vH_th
         safety = not is_occupied(x['pH'][i], x['lH'][i] - 1)
-        # dH.append(0 if fol is None 
-        #           else -(incentive and safety))
         global t_switch
         if i == 1 and safety and t_switch == -1:
             t_switch = t
         dH.append(0 if fol is None else -safety)
-        # dH.append(0)
         
     return (aH, dH)
 
-def compute_u():
+def compute_u(): # compute robot car controls
 
+    # get constraint parameters
     A11, A12, b1 = hF.constraint(x)
     A21, A22, b2 = hB.constraint(x)
     print(A11, A12, b1, A21, A22, b2)
 
+    # Ax >= b
     A = cvxopt.matrix([[-A11, -A21], [-A12, -A22]])
     print(A)
     b = cvxopt.matrix([-b1, -b2])
     print(b)
 
+    # minimize ||u||^2 (control effort) s.t. constraints
     u_star = [0 for _ in range(n)]
-    sol = cvxopt.solvers.qp(cvxopt.matrix([[1.0, 0.0], [0.0, 1.0]]), # 1.0 for min, -1.0 for max
+    sol = cvxopt.solvers.qp(cvxopt.matrix([[1.0, 0.0], [0.0, 1.0]]),
                             cvxopt.matrix([0.0, 0.0]), 
                             A, b)
     if sol['status'] == 'optimal':
         u_star = sol['x']
 
+    # if objective already satisfied, passive driving
     if x['lH'][1] == -1:
         u_star[0] = idm.free_road(x['vR'][0])
         u_star[1] = idm.free_road(x['vR'][1])
@@ -346,6 +291,7 @@ def compute_u():
 
 if __name__ == '__main__':
 
+    # set up initial conditions
     x['pH'].append(-2*L)
     x['vH'].append(30)
     x['aH'].append(0)
@@ -368,9 +314,11 @@ if __name__ == '__main__':
     x['aR'].append(0)
     x['lR'].append(0)
 
+    # initialize cbf functions
     hF = CBF_hF31(x, s_min, L, dt, c_min, idm)
     hB = CBF_hB31(x, s_min, L, dt, c_min, idm)
 
+    # run simulation
     u = ([0 for _ in range(n)], [0 for _ in range(n)])
     while t <= t_max:
         apply_control(compute_u())
