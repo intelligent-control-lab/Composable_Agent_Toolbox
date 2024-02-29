9b7e193c10391041321e3a4703a6c9781845c2b1
Anirudh Chari <anirudh.chari05@gmail.com>
2024-02-29 13:13:46 -0600

    create highway sim class

--- examples/comm.py
+++ examples/comm.py
@@ -1,12 +1,7 @@
+from highway_simulator import HighwaySimulator
 import ortoolpy
 import random
-import copy
-import math
-import random
 from IDM import IDM
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle
 
 m = 2
 n = 2
@@ -20,21 +15,9 @@ v = [21, 11, 15, 9, 34, 25, 41, 52]
 c = [22, 12, 16, 10, 35, 26, 42, 53]
 print(ortoolpy.knapsack(v, c, beta))
 
-
-
-dt = 0.1
-t_max = 40
-
 L = 5
-
-x = {'pH': [], 'vH': [], 'aH': [], 'lH': [], 'dH': [], 
-     'pR': [], 'vR': [], 'aR': [], 'lR': [], 
-     'pB': [], 'vB': [], 'lB': []}
-
 a_max = 2.0
 b_max = 4.0
-vR_max = [35, 35]
-
 s0 = 0.3*L
 v0 = 35
 vH_th = 10
@@ -42,177 +25,46 @@ T = 0.01 # 1.3
 a = 1.8
 b = 3.1
 
-idmH = [IDM(random.uniform(0.5, 1.5), random.uniform(20, 30), 
-            T, a, b, L) for _ in range(m)]
-idmR = [IDM(random.uniform(0.5, 1.5), random.uniform(20, 30), 
-            T, a, b, L) for _ in range(n)]
-
-x_all = []
-
-t = 0
-
-fig, ax = plt.subplots()
-
-def vis(pov):
-    
-    ax.cla()
-    ax.axis([-4*L, 4*L, pov - 4*L, pov + 4*L])
-    ax.axvline(x=-L/2, color="black", linestyle="--")
-    ax.axvline(x=L/2, color="black", linestyle="--")
-    ax.axvline(x=-3*L/2, color="black", linestyle="-")
-    ax.axvline(x=3*L/2, color="black", linestyle="-")
-
-    ax.text(-4*L, pov + 4*L, "t = " + str(round(t, 1)))
-    for i in range(m):
-        ax.text(x['lH'][i]*L, x['pH'][i], str(round(x['vH'][i], 1)))
-    for i in range(n):
-        ax.text(x['lR'][i]*L, x['pR'][i], str(round(x['vR'][i], 1)))
-    for i in range(q):
-        ax.text(x['lB'][i]*L, x['pB'][i], str(round(x['vB'][i], 1)))
-
-    for i in range(m):
-        ax.add_patch(Rectangle(
-            (x['lH'][i]*L - L/4, x['pH'][i] - L/2), 
-            L/2, L, 
-            facecolor='blue'))
-    for i in range(n):
-        ax.add_patch(Rectangle(
-            (x['lR'][i]*L - L/4, x['pR'][i] - L/2), 
-            L/2, L, 
-            facecolor='red'))
-    for i in range(q):
-        ax.add_patch(Rectangle(
-            (x['lB'][i]*L - L/4, x['pB'][i] - L/2), 
-            L/2, L, 
-            facecolor='green'))
-
-    plt.pause(0.001)
-
-def get_following(i, l):
-    fol = -1
-    for j in range(m):
-        if j == i or x['lH'][j] != l or x['pH'][i] > x['pH'][j]:
-            continue
-        if fol == -1 or x['pH'][j] < x['pH'][fol]:
-            fol = j
-    if fol != -1:
-        return x['pH'][fol], x['vH'][fol]
-    for j in range(n):
-        if x['lR'][j] != l or x['pH'][i] > x['pR'][j]:
-            continue
-        if fol == -1 or x['pR'][j] < x['pR'][fol]:
-            fol = j
-    if fol != -1:
-        return x['pR'][fol], x['vR'][fol]
-    for j in range(q):
-        if x['lB'][j] != l or x['pH'][i] > x['pB'][j]:
-            continue
-        if fol == -1 or x['pB'][j] < x['pB'][fol]:
-            fol = j
-    if fol != -1:
-        return x['pB'][fol], x['vB'][fol]
-    return None
-
-def is_occupied(p, l):
-    for i in range(m):
-        if x['lH'][i] == l and abs(p - x['pH'][i]) < L + L:
-            return True
-    for j in range(n):
-        if x['lR'][j] == l and abs(p - x['pR'][j]) < L + L:
-            return True
-    for k in range(q):
-        if x['lB'][k] == l and abs(p - x['pB'][k]) < L + L:
-            return True
-    return False
-
-def f():
-
-    aH = []
-    dH = []
-
-    for i in range(m):
-        fol = get_following(i, x['lH'][i])
-        aH.append(idmH[i].free_road(x['vH'][i]) if fol is None 
-                  else idmH[i].idm(x['pH'][i], fol[0], x['vH'][i], fol[1]))
-        incentive = False
-        if fol is not None:
-            fol_new = get_following(i, -1)
-            vel_new = v0 if fol_new is None else fol_new[1]
-            incentive = vel_new - fol[1] >= vH_th
-        safety = not is_occupied(x['pH'][i], x['lH'][i] - 1)
-        # dH.append(0 if fol is None 
-        #           else -(incentive and safety))
-        dH.append(0 if fol is None else -safety)
-        
-    return (aH, dH)
-
-def u():
-
-    aR = []
-    dR = [0 for _ in range(n)]
-
-    for i in range(n):
-        fol = get_following(i, x['lR'][i])
-        aR.append(idmR[i].free_road(x['vR'][i]) if fol is None 
-                  else idmR[i].idm(x['pR'][i], fol[0], x['vR'][i], fol[1]))
-    
-    return (aR, dR)
-
-def move(idm=True):
-
-    aH, dH = f() if idm else ([0 for _ in range(m)], 
-                              [0 for _ in range(m)])
-    aR, dR = u() if idm else ([0 for _ in range(n)], 
-                              [0 for _ in range(n)])
-
-    for i in range(m):
-        aH[i] = min(a_max, max(-b_max, aH[i]))
-        x['vH'][i] += aH[i] * dt
-        x['pH'][i] += x['vH'][i] * dt
-        x['lH'][i] = max(-1, min(1, 
-            x['lH'][i] + round(dH[i])))
-        
-    for i in range(n):
-        aR[i] = min(a_max, max(-b_max, aR[i]))
-        x['vR'][i] += aR[i] * dt
-        x['vR'][i] = min(vR_max[i], x['vR'][i])
-        x['pR'][i] += x['vR'][i] * dt
-        x['lR'][i] = max(-1, min(1, 
-            x['lR'][i] + round(dR[i])))
-        
-    for i in range(q):
-        x['pB'][i] += x['vB'][i] * dt
-        
-    x['aH'] = aH
-    x['aR'] = aR
-    x_all.append((t, copy.deepcopy(x)))
-
 if __name__ == '__main__':
 
-    x['pH'].append(-3*L)
-    x['vH'].append(20)
-    x['aH'].append(0)
-    x['lH'].append(-1)
-    x['dH'].append(0)
+    x_init = {'pH': [], 'vH': [], 'aH': [], 'lH': [], 'dH': [], 
+     'pR': [], 'vR': [], 'aR': [], 'lR': [], 
+     'pB': [], 'vB': [], 'lB': []}
 
-    x['pH'].append(-2*L)
-    x['vH'].append(20)
-    x['aH'].append(0)
-    x['lH'].append(0)
-    x['dH'].append(0)
+    x_init['pH'].append(-3*L)
+    x_init['vH'].append(20)
+    x_init['aH'].append(0)
+    x_init['lH'].append(-1)
+    x_init['dH'].append(0)
 
-    x['pR'].append(0)
-    x['vR'].append(20)
-    x['aR'].append(0)
-    x['lR'].append(-1)
+    x_init['pH'].append(-2*L)
+    x_init['vH'].append(20)
+    x_init['aH'].append(0)
+    x_init['lH'].append(0)
+    x_init['dH'].append(0)
 
-    x['pR'].append(0)
-    x['vR'].append(20)
-    x['aR'].append(0)
-    x['lR'].append(0)
+    x_init['pR'].append(0)
+    x_init['vR'].append(20)
+    x_init['aR'].append(0)
+    x_init['lR'].append(-1)
 
-    # u = ([0 for _ in range(n)], [0 for _ in range(n)])
+    x_init['pR'].append(0)
+    x_init['vR'].append(20)
+    x_init['aR'].append(0)
+    x_init['lR'].append(0)
+
+    idmH = [IDM(random.uniform(0.5, 1.5), random.uniform(20, 30), 
+            T, a, b, L) for _ in range(m)]
+    idmR = [IDM(random.uniform(0.5, 1.5), random.uniform(20, 30), 
+            T, a, b, L) for _ in range(n)]
+
+    t = 0
+    t_max = 40
+    dt = 0.1
+
+    sim = HighwaySimulator(x_init, m, n, q, L, idmH, idmR, dt)
+    
     while t <= t_max:
-        move(idm=True)
-        vis(x['pH'][1])
+        sim.move(use_idm=True)
+        sim.vis(t, sim.x['pH'][1])
         t += dt
---
+++ examples/highway_simulator.py
@@ -0,0 +1,141 @@
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
+class HighwaySimulator:
+    def __init__(self, x_init, m, n, q, L, idmH, idmR, dt):
+        self.x = x_init
+        self.m = m
+        self.n = n
+        self.q = q
+        self.L = L
+        self.idmH = idmH
+        self.idmR = idmR
+        self.dt = dt
+
+        self.fig, self.ax = plt.subplots()
+
+    def vis(self, t, pov):
+        
+        self.ax.cla()
+        self.ax.axis([-4*self.L, 4*self.L, pov - 4*self.L, pov + 4*self.L])
+        self.ax.axvline(x=-self.L/2, color="black", linestyle="--")
+        self.ax.axvline(x=self.L/2, color="black", linestyle="--")
+        self.ax.axvline(x=-3*self.L/2, color="black", linestyle="-")
+        self.ax.axvline(x=3*self.L/2, color="black", linestyle="-")
+
+        self.ax.text(-4*self.L, pov + 4*self.L, "t = " + str(round(t, 1)))
+        for i in range(self.m):
+            self.ax.text(self.x['lH'][i]*self.L, self.x['pH'][i], str(round(self.x['vH'][i], 1)))
+        for i in range(self.n):
+            self.ax.text(self.x['lR'][i]*self.L, self.x['pR'][i], str(round(self.x['vR'][i], 1)))
+        for i in range(self.q):
+            self.ax.text(self.x['lB'][i]*self.L, self.x['pB'][i], str(round(self.x['vB'][i], 1)))
+
+        for i in range(self.m):
+            self.ax.add_patch(Rectangle(
+                (self.x['lH'][i]*self.L - self.L/4, self.x['pH'][i] - self.L/2), 
+                self.L/2, self.L, 
+                facecolor='blue'))
+        for i in range(self.n):
+            self.ax.add_patch(Rectangle(
+                (self.x['lR'][i]*self.L - self.L/4, self.x['pR'][i] - self.L/2), 
+                self.L/2, self.L, 
+                facecolor='red'))
+        for i in range(self.q):
+            self.ax.add_patch(Rectangle(
+                (self.x['lB'][i]*self.L - self.L/4, self.x['pB'][i] - self.L/2), 
+                self.L/2, self.L, 
+                facecolor='green'))
+
+        plt.pause(0.001)
+
+    def get_following(self, i, l):
+        fol = -1
+        for j in range(self.m):
+            if j == i or self.x['lH'][j] != l or self.x['pH'][i] > self.x['pH'][j]:
+                continue
+            if fol == -1 or self.x['pH'][j] < self.x['pH'][fol]:
+                fol = j
+        if fol != -1:
+            return self.x['pH'][fol], self.x['vH'][fol]
+        for j in range(self.n):
+            if self.x['lR'][j] != l or self.x['pH'][i] > self.x['pR'][j]:
+                continue
+            if fol == -1 or self.x['pR'][j] < self.x['pR'][fol]:
+                fol = j
+        if fol != -1:
+            return self.x['pR'][fol], self.x['vR'][fol]
+        for j in range(self.q):
+            if self.x['lB'][j] != l or self.x['pH'][i] > self.x['pB'][j]:
+                continue
+            if fol == -1 or self.x['pB'][j] < self.x['pB'][fol]:
+                fol = j
+        if fol != -1:
+            return self.x['pB'][fol], self.x['vB'][fol]
+        return None
+
+    def is_occupied(self, p, l):
+        for i in range(self.m):
+            if self.x['lH'][i] == l and abs(p - self.x['pH'][i]) < self.L + self.L:
+                return True
+        for j in range(self.n):
+            if self.x['lR'][j] == l and abs(p - self.x['pR'][j]) < self.L + self.L:
+                return True
+        for k in range(self.q):
+            if self.x['lB'][k] == l and abs(p - self.x['pB'][k]) < self.L + self.L:
+                return True
+        return False
+
+    def f(self):
+        aH = []
+        dH = []
+        for i in range(self.m):
+            fol = self.get_following(i, self.x['lH'][i])
+            aH.append(self.idmH[i].free_road(self.x['vH'][i]) if fol is None 
+                    else self.idmH[i].idm(self.x['pH'][i], fol[0], self.x['vH'][i], fol[1]))
+            # incentive = False
+            # if fol is not None:
+            #     fol_new = get_following(i, -1)
+            #     vel_new = v0 if fol_new is None else fol_new[1]
+            #     incentive = vel_new - fol[1] >= vH_th
+            safety = not self.is_occupied(self.x['pH'][i], self.x['lH'][i] - 1)
+            # dH.append(0 if fol is None 
+            #           else -(incentive and safety))
+            dH.append(0 if fol is None else -safety)
+        return (aH, dH)
+
+    def u(self):
+        aR = []
+        dR = [0 for _ in range(self.n)]
+        for i in range(self.n):
+            fol = self.get_following(i, self.x['lR'][i])
+            aR.append(self.idmR[i].free_road(self.x['vR'][i]) if fol is None 
+                    else self.idmR[i].idm(self.x['pR'][i], fol[0], self.x['vR'][i], fol[1]))
+        return (aR, dR)
+
+    def move(self, use_idm=True):
+
+        aH, dH = self.f() if use_idm else ([0 for _ in range(self.m)], 
+                                [0 for _ in range(self.m)])
+        aR, dR = self.u() if use_idm else ([0 for _ in range(self.n)], 
+                                [0 for _ in range(self.n)])
+
+        for i in range(self.m):
+            # aH[i] = min(a_max, max(-b_max, aH[i]))
+            self.x['vH'][i] += aH[i] * self.dt
+            self.x['pH'][i] += self.x['vH'][i] * self.dt
+            self.x['lH'][i] = max(-1, min(1, 
+                self.x['lH'][i] + round(dH[i])))
+            
+        for i in range(self.n):
+            # aR[i] = min(a_max, max(-b_max, aR[i]))
+            self.x['vR'][i] += aR[i] * self.dt
+            self.x['pR'][i] += self.x['vR'][i] * self.dt
+            self.x['lR'][i] = max(-1, min(1, 
+                self.x['lR'][i] + round(dR[i])))
+            
+        for i in range(self.q):
+            self.x['pB'][i] += self.x['vB'][i] * self.dt
+            
+        self.x['aH'] = aH
+        self.x['aR'] = aR
